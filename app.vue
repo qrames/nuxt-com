d173cddd0630ec781ec2cf00113023066a1b62f4
Quentin <ramque@orange.fr>
2024-03-24 21:02:16 +0100

    install flowbite

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
+<script setup>
+  import { onMounted } from 'vue'
+  import { initFlowbite } from 'flowbite'
+
+  // initialize components based on data attribute selectors
+  onMounted(() => {
+      initFlowbite()
+  })
+</script>
 <template>
-  <div>
+  <NuxtLayout>
     <NuxtWelcome />
-  </div>
+  </NuxtLayout>
 </template>
