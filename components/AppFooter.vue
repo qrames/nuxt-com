08dc3691a4395171bb39fa2f4bc8b5c5959005cf
Quentin <ramque@orange.fr>
2024-03-24 21:04:23 +0100

    first nav bar and layout

---
+++ components/AppFooter.vue
@@ -0,0 +1,3 @@
+<template>
+    © Quentin Rames
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <header>
+      <slot name="header">
+        <AppHeader />
+      </slot>
+    </header>
+    <main>
+      <slot />
+    </main>
+    <footer>
+      <slot name="footer">
+          <AppFooter />
+      </slot>
+    </footer>
+  </div>
+</template>
