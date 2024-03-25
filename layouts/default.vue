87da607447542a946840fd17f4bfe7a1e8b9d314
Quentin <ramque@orange.fr>
2024-03-25 15:39:58 +0100

    main template add centre div style

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,11 @@
       </slot>
     </header>
     <main>
-      <slot />
+      <div class="antialiased bg-white dark:bg-black text-black dark:text-white min-h-screen place-content-center flex flex-col items-center justify-center text-sm sm:text-base">
+        <div class="flex flex-col gap-y-4 items-center justify-center">
+          <slot />
+        </div>
+      </div>
     </main>
     <footer>
       <slot name="footer">
