66ced25715ab4f3929422dfb702385c98502d2c7
Quentin <ramque@orange.fr>
2024-03-25 15:37:35 +0100

    install pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ]
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  modules: ['@nuxt/ui', '@pinia/nuxt']
 })
