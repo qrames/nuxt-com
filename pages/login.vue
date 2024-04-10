26755f38bb022311cd59f4ae4442a699268a0fef
Quentin <ramque@orange.fr>
2024-04-11 01:14:29 +0200

    add btn cgu (rgpd) /don't work

--- pages/login.vue
+++ pages/login.vue
@@ -33,6 +33,12 @@
             </div>
             <label for="remember-me" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">remember me.</label>
         </div>
+        <div class="flex items-start mb-6">
+            <div class="flex items-center h-5">
+            <input id="consent-cgu" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"/>
+            </div>
+            <label for="consent-cgu" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">consent cgu.</label>
+        </div>
         <p>10000000-ffff-ffff-ffff-000000000001 sitekey hcaptcha dev</p>
         <div id="captcha-1" class="h-captcha" data-sitekey="" data-theme="dark"></div> 
         <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
