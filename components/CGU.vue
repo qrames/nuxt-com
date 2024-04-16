e87552f2e97120bc34fd3f49abc82e21dba5cc78
Quentin <ramque@orange.fr>
2024-04-16 19:36:33 +0200

    CGU page

---
+++ components/CGU.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1 class="text-6xl text-amber-950">CGU</h1>
+    <p class="text-2xl text-amber-950">Conditions Générales d'Utilisation</p>
+
+    <h2 class="text-4xl text-amber-950">1. Préambule</h2>
+    <p>Ces conditions générales d'utilisation (ci-après "CGU") s'appliquent à tout visiteur du site www.votresite.com (ci-après "le Site"). L'utilisation du Site implique l'acceptation intégrale et sans réserve des CGU.</p>
+
+    <h2 class="text-4xl text-amber-950">2. Informations légales</h2>
+    <p>Le Site est édité par la société [Nom de votre société], une société [forme juridique] inscrite au RCS de [ville] sous le numéro [numéro], dont le siège social est situé à [adresse].</p>
+
+    <h2 class="text-4xl text-amber-950">3. Accès au site</h2>
+    <p>Le Site est accessible gratuitement à tout utilisateur disposant d'un accès à internet.</p>
+
+    <h2 class="text-4xl text-amber-950">4. Propriété intellectuelle</h2>
+    <p>Tous les éléments du Site, qu'ils soient visuels ou sonores, y compris la technologie sous-jacente, sont protégés par le droit d'auteur, des marques ou des brevets.</p>
+
+    <h2 class="text-4xl text-amber-950">5. Données personnelles</h2>
+    <p>Les informations recueillies sur le Site sont traitées conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés et au Règlement (UE) 2016/679 du Parlement européen et du Conseil du 27 avril 2016.</p>
+
+    <h2 class="text-4xl text-amber-950">6. Responsabilité</h2>
+    <p>[Nom de votre société] ne peut être tenu responsable des dommages directs ou indirects causés au matériel de l'utilisateur lors de l'accès au Site.</p>
+
+    <h2 class="text-4xl text-amber-950">7. Modification des CGU</h2>
+    <p>[Nom de votre société] se réserve le droit de modifier les CGU à tout moment. L'utilisateur sera informé de ces modifications par tout moyen utile.</p>
+
+    <h2 class="text-4xl text-amber-950">8. Droit applicable et juridiction</h2>
+    <p>Les CGU sont soumises au droit français. En cas de litige, les tribunaux français seront seuls compétents.</p>
+
+    <p>Date de dernière mise à jour : [date]</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
+  
---
+++ pages/cgus.vue
@@ -0,0 +1,3 @@
+<template>
+    <CGU></CGU>
+</template>
